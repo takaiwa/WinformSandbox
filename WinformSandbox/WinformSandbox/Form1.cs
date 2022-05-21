1cddc6b2098f446b44b428f2abbbbcf808ef2d94
Takaiwa29 <devtakaiwa@gmail.com>
2022-05-21 10:51:01 +0900

    ProgressBarの確認

---
+++ WinformSandbox/WinformSandbox/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformSandbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.progressBar1.Visible = true;
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Maximum = 10;
+            this.progressBar1.Value = 1;
+            this.progressBar1.Step = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.progressBar1.PerformStep();
+        }
+    }
+}
