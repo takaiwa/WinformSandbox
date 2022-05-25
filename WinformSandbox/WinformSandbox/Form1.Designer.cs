8611aa541a1bca5d9b3449a50c2f290e168fd3e2
Takaiwa29 <devtakaiwa@gmail.com>
2022-05-25 23:21:46 +0900

    進捗を非同期処理にした

--- WinformSandbox/WinformSandbox/Form1.Designer.cs
+++ WinformSandbox/WinformSandbox/Form1.Designer.cs
@@ -30,7 +30,10 @@ namespace WinformSandbox
         private void InitializeComponent()
         {
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.labelProgress = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // progressBar1
@@ -40,33 +43,64 @@ namespace WinformSandbox
             this.progressBar1.Size = new System.Drawing.Size(304, 23);
             this.progressBar1.TabIndex = 0;
             // 
-            // button1
+            // btnCancel
             // 
-            this.button1.Location = new System.Drawing.Point(261, 144);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnCancel.Location = new System.Drawing.Point(261, 144);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorker_DoWork);
+            this.bgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BgWorker_ProgressChanged);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(32, 144);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Location = new System.Drawing.Point(41, 64);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(35, 12);
+            this.labelProgress.TabIndex = 3;
+            this.labelProgress.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(361, 182);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.progressBar1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label labelProgress;
     }
 }
 
--- WinformSandbox/WinformSandbox/Form1.cs
+++ WinformSandbox/WinformSandbox/Form1.cs
@@ -1,31 +1,123 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace WinformSandbox
+﻿namespace WinformSandbox
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
+
+        private static int maxValue = 10;
+
         public Form1()
         {
-            InitializeComponent();
-
-            this.progressBar1.Visible = true;
-            this.progressBar1.Minimum = 1;
-            this.progressBar1.Maximum = 10;
-            this.progressBar1.Value = 1;
-            this.progressBar1.Step = 1;
+            this.InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        /// <summary>
+        /// 処理開始.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnStart_Click(object sender, EventArgs e)
         {
+            this.progressBar1.Visible = true;
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Maximum = maxValue;
+            this.progressBar1.Value = 1;
+            this.progressBar1.Step = 1;
+
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.WorkerSupportsCancellation = true;
+
+            // 処理開始
+            this.bgWorker.RunWorkerAsync(maxValue);
+        }
+
+        /// <summary>
+        /// 時間のかかる処理をここで行う.
+        /// </summary>
+        /// <param name="sender">BackgroundWorker.</param>
+        /// <param name="e"></param>
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgWorker = (BackgroundWorker)sender;
+            int max = (int)e.Argument;
+
+            Console.WriteLine(string.Format("max:{0}", max));
+
+            for (int i = 1; i < max; i++)
+            {
+                if (bgWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
+                // 画面の表示を変更する
+                bgWorker.ReportProgress(i);
+
+                // 1000ms待機で疑似重い処理
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            e.Result = 1;
+        }
+
+        /// <summary>
+        /// 画面の更新.
+        /// ※DoWorkで内では絶対に行わない.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">進捗が入っている.</param>
+        private void BgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // プログレスバー更新
             this.progressBar1.PerformStep();
+
+            // 進捗を表示
+            this.labelProgress.Text = string.Format("{0}%", e.ProgressPercentage.ToString());
+        }
+
+        /// <summary>
+        /// 処理が終わったら実行される.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">結果が格納されている.</param>
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                // エラーが発生したとき
+                this.labelProgress.Text = "エラー:" + e.Error.Message;
+            }
+            else if (e.Cancelled)
+            {
+                // キャンセルされたとき
+                this.labelProgress.Text = "キャンセルされました";
+            }
+            else
+            {
+                // 正常に終了したとき→結果を表示
+                int result = (int)e.Result;
+                this.labelProgress.Text = string.Format("結果{0}", result.ToString());
+            }
+        }
+
+        /// <summary>
+        /// キャンセル処理.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.bgWorker.CancelAsync();
         }
     }
 }
